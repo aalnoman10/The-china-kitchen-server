0e540183fb86466b77aa01cb2faa83653cc84791
aalnoman10 <aalnomanm10@gmail.com>
2023-05-02 23:54:17 +0600

    update index file to send dynamic item

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
     res.send(chefs)
 })
 
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const selected = chefs.find(chef => chef.id === id)
+    res.send(selected)
+})
+
 app.listen(port, () => {
     console.log(`server in run port ${port}`);
 })
